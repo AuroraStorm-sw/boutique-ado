30c53aa2619d1f699275c8f183a9dd086e007a90
Aurora <aurorastorm22@proton.me>
2023-08-21 07:41:58 +0000

    completed home page header and css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7g_ukfs3kryq*i^plp)ie2-n00p_-kaqq6*d@ll=flx8y4qtpd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-aurorastorm-boutiqueado-clw1gy115vk.ws-eu103.gitpod.io']
+ALLOWED_HOSTS = ['8000-aurorastorm-boutiqueado-clw1gy115vk.ws-eu104.gitpod.io']
 
 
 # Application definition
@@ -146,6 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
